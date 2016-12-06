ca61c8d246dbb0a080c2359bfca9302a22e434ae
Scott McCord <scott.sdm27@gmail.com>
2016-12-06 17:22:29 -0500

    created route

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const path = require('path');
 
+// const homeRoute = require('./routes/index.js')
+
 // Only load dotenv if we need it (if we have NODE_ENV in our environment)
 const isDev = !('NODE_ENV' in process.env) && require('dotenv').config() && true;
 
@@ -17,6 +19,9 @@ app.use(logger(isDev ? 'dev' : 'common'));
 
 // accept JSON formatted data
 app.use(bodyParser.json());
+
+// handle the routes
+// app.use('/', homeRoute);
 
 // add an error handler
 app.use((err, req, res, next) => {
