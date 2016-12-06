013f7dd6ecaeb67689ca789f1be6c9c652ede757
Scott McCord <scott.sdm27@gmail.com>
2016-12-06 17:51:14 -0500

    set up App component

---
+++ src/components/app/App.jsx
@@ -0,0 +1,14 @@
+// import the libraries we need
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// create react component for App
+export default class App extends React.Component{
+  render(){
+    return(
+      <container>
+        <h1> Hello, and welcome to React. </h1>
+      </container>
+    )
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// mount our App at #container
+ReactDOM.render(<App />, document.querySelector('#container'));
