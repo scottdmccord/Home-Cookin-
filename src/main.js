99b3b2472744ef308da1ec4ccc9c38cd59aabcae
Scott McCord <scott.sdm27@gmail.com>
2016-12-07 08:48:25 -0500

    created basic React Router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App'
+import App from './components/App/App';
+import { Router, Route, hashHistory } from 'react-router';
+
+// I followed the React Router tutorial to render different
+// pages through my App: https://github.com/reactjs/react-router-tutorial
 
 // mount our App at #container
-ReactDOM.render(<App />, document.querySelector('#container'));
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+), document.querySelector('#container'));
