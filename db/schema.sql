47ce12f3d71c697ccd8167b2bad1247d4216ddf4
Scott McCord <scott.sdm27@gmail.com>
2016-12-08 17:14:58 -0500

    finished schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,19 +2,22 @@ DROP TABLE IF EXISTS cooks;
 DROP TABLE IF EXISTS consumers;
 DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS meal_counter;
 
 CREATE TABLE cooks (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
   email VARCHAR NOT NULL,
-  password VARCHAR NOT NULL
+  password VARCHAR NOT NULL,
+  date_created TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE consumers (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
   email VARCHAR NOT NULL,
-  password VARCHAR NOT NULL
+  password VARCHAR NOT NULL,
+  date_created TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE meals (
@@ -27,14 +30,58 @@ CREATE TABLE meals (
   pickup_time TIME NOT NULL,
   pickup_address TEXT NOT NULL,
   price INT NOT NULL,
-  cook_id INT NOT NULL
+  cook_id INT NOT NULL,
+  date_created TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE reviews (
   id SERIAL PRIMARY KEY,
   cook_id INT NOT NULL,
-  user_id INT NOT NULL,
+  consumer_id INT NOT NULL,
   rating INT NOT NULL,
   review TEXT NOT NULL,
-  date DATE NOT NULL
+  date DATE NOT NULL,
+  date_created TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE meal_counter (
+  id SERIAL PRIMARY KEY,
+  meal_id INT NOT NULL,
+  consumer_id INT NOT NULL,
+  date_created TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+ALTER TABLE ONLY meals
+  ADD CONSTRAINT meals_cook_id_fkey
+  FOREIGN KEY (cook_id)
+  REFERENCES cooks(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+
+ALTER TABLE ONLY reviews
+  ADD CONSTRAINT reviews_cook_id_fkey
+  FOREIGN KEY (cook_id)
+  REFERENCES cooks(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+
+ALTER TABLE ONLY reviews
+  ADD CONSTRAINT reviews_consumer_id_fkey
+  FOREIGN KEY (consumer_id)
+  REFERENCES consumers(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+
+ALTER TABLE ONLY meal_counter
+  ADD CONSTRAINT meal_counter_consumer_id_fkey
+  FOREIGN KEY (consumer_id)
+  REFERENCES consumers(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+
+ALTER TABLE ONLY meal_counter
+  ADD CONSTRAINT meal_counter_meal_id_fkey
+  FOREIGN KEY (meal_id)
+  REFERENCES meals(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
