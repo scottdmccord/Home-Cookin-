464a1dfdd9669c742ba6b40f8b4d0dcd9cf0ebf4
Scott McCord <scott.sdm27@gmail.com>
2016-12-10 11:26:36 -0500

    consumer auth router set up

--- routes/consumers.js
+++ routes/consumers.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { createConsumer } = require('../models/consumers.js');
+const { createConsumer, authenticateConsumer } = require('../models/consumers.js');
 
 const consumersRouter = express.Router();
 
 consumersRouter.post('/', createConsumer, (req, res) => {
   res.redirect('/');
+});
+
+consumersRouter.post('/login', authenticateConsumer, (req, res, next) => {
+  res.json({message: "successfully signed in"});
 });
 
 module.exports = consumersRouter;
