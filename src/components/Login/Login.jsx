3a1adde5f3ddc89fc1678973661b2fb95b1b46f3
Scott McCord <scott.sdm27@gmail.com>
2016-12-10 11:44:05 -0500

    added consumer login to login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,14 +8,22 @@ class Login extends Component {
       cookLogIn: {
         username: '',
         password: ''
+      },
+      consumerLogIn: {
+        username: '',
+        password: ''
       }
     }
-  this.updateLogInUsername = this.updateLogInUsername.bind(this);
-  this.updateLogInPassword = this.updateLogInPassword.bind(this);
+
+  this.updateLogInUsername = this.updateCookLogInUsername.bind(this);
+  this.updateLogInPassword = this.updateCookLogInPassword.bind(this);
   this.cooksLogin = this.cooksLogin.bind(this);
+
   }
 
-    updateLogInUsername(e){
+
+    // COOKS login and authorization
+    updateCookLogInUsername(e){
       console.log(e.target.value)
       this.setState({
         cookLogIn: {
@@ -25,7 +33,7 @@ class Login extends Component {
       });
     }
 
-    updateLogInPassword(e){
+    updateCookLogInPassword(e){
       console.log(e.target.value)
       this.setState({
         cookLogIn: {
@@ -50,7 +58,7 @@ class Login extends Component {
       })
       .then(r => r.json())
       .then((data) => {
-          this.props.updateCurrentToken(data);
+        this.props.updateCurrentToken(data);
         this.setState({
           cookLogIn: {
             username: '',
@@ -62,11 +70,57 @@ class Login extends Component {
       })
     }
 
+    // CONSUMERS login and authorization
+    updateConsumerLogInUsername(e) {
+      console.log(e.target.value)
+      this.setState({
+        consumerLogIn: {
+          username: e.target.value,
+          password: this.state.consumerLogIn.password
+        }
+      });
+    }
+
+    updateConsumerLogInPassword(e) {
+      console.log(e.target.value);
+      this.setState({
+        consumerLogIn: {
+          username: this.state.consumerLogIn.username,
+          password: e.target.value
+        }
+      });
+    }
+
+    consumersLogin() {
+      console.log('posting the consumer login!');
+      fetch('/consumers/login', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: this.state.consumerLogIn.username,
+          password: this.state.consumerLogIn.password
+        })
+      })
+      .then(r => r.json())
+      .then((data) => {
+        this.props.updateCurrentToken(data);
+        this.setState({
+          consumerLogIn: {
+            username: '',
+            password: ''
+          }
+        }, () => {
+          console.log(this.state);
+        })
+      })
+    }
+
   render() {
     return(
       <container>
         <h1> COOK LOGIN </h1>
-
 
           <div className="login-username-div">
             <label> Username: </label>
@@ -75,7 +129,7 @@ class Login extends Component {
               type="text"
               placeholder="Enter Username"
               value={this.state.cookLogIn.username}
-              onChange={this.updateLogInUsername}
+              onChange={this.updateCookLogInUsername}
             />
           </div>
 
@@ -86,13 +140,38 @@ class Login extends Component {
               type="text"
               placeholder="Enter Password"
               value={this.state.cookLogIn.password}
-              onChange={this.updateLogInPassword}
+              onChange={this.updateCookLogInPassword}
             />
           </div>
 
           <button onClick={this.cooksLogin}>Enter</button>
 
         <h1> CONSUMER LOGIN </h1>
+
+        <div className="login-username-div">
+          <label> Username: </label>
+          <input
+            className="login username"
+            type="text"
+            placeholder="Enter Username"
+            value={this.state.consumerLogIn.username}
+            onChange={this.updateConsumerLogInUsername}
+          />
+        </div>
+
+        <div className="login-password-div">
+          <label> Password: </label>
+          <input
+            className="login password"
+            type="text"
+            placeholder="Enter Password"
+            value={this.state.consumerLogIn.password}
+            onChange={this.updateConsumerLogInPassword}
+          />
+        </div>
+
+        <button onClick={this.consumersLogin}>Enter</button>
+
       </container>
     )
   }
