454e0008be509638ec35e46d5637ae2511208842
Scott McCord <scott.sdm27@gmail.com>
2016-12-10 17:10:42 -0500

    added background image to landing component

--- src/components/CookSignUp/CookSignUp.jsx
+++ src/components/CookSignUp/CookSignUp.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './CookSignUp.css';
 
 class CookSignUp extends Component {
   constructor(props) {
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -5,7 +5,7 @@ import style from './Landing.css';
 class Landing extends Component {
   render() {
     return(
-      <div>
+      <div className="landingBody">
         <div className="cook signup"><Link to="/cookSignUp">Sign up as cook</Link></div>
         <div className="consumer signup"><Link to="/consumerSignUp">Sign up as consumer</Link></div>
       </div>
