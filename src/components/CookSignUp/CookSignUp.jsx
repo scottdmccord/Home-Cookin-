a99280edc64029fac67302a9a86c4fa64f1de889
Scott McCord <scott.sdm27@gmail.com>
2016-12-13 10:56:17 -0500

    added more styling to cook signup page

--- src/components/CookSignUp/CookSignUp.jsx
+++ src/components/CookSignUp/CookSignUp.jsx
@@ -90,82 +90,87 @@ class CookSignUp extends Component {
       <container className="cook-signup-page">
 
         <div className="signup-content">
-        <h1> COOK SIGN UP </h1>
-
-        <div className="signup-container">
-
-          <div className="signup-name-div">
-            <label> Name: </label>
-            <input
-              className="signup-input name"
-              type="text"
-              placeholder="Enter Name"
-              value={this.state.cookSignUpName}
-              onChange={this.updateSignUpName}
-            />
+          <div className="cook-signup-header">
+            <h1 className="signup-title"> COOK SIGN UP </h1>
           </div>
 
-          <div className="signup-address-div">
-            <label> Address: </label>
-            <input
-              className="signup-input address"
-              type="text"
-              placeholder="Enter address"
-              value={this.state.cookSignUpAddress}
-              onChange={this.updateSignUpAddress}
-            />
+          <div className="signup-container">
+            <div className="signup-second-container">
+
+            <div className="signup-input-div">
+              <label> Name: </label>
+              <input
+                className="signup-input name"
+                type="text"
+                placeholder="Enter Name"
+                value={this.state.cookSignUpName}
+                onChange={this.updateSignUpName}
+              />
+            </div>
+
+            <div className="signup-input-div">
+              <label> Address: </label>
+              <input
+                className="signup-input address"
+                type="text"
+                placeholder="Enter address"
+                value={this.state.cookSignUpAddress}
+                onChange={this.updateSignUpAddress}
+              />
+            </div>
+
+            <div className="signup-input-div">
+              <label> Neighborhood: </label>
+              <input
+                className="signup-input neighborhood"
+                type="text"
+                placeholder="Enter Neighborhood"
+                value={this.state.cookSignUpNeighborhood}
+                onChange={this.updateSignUpNeighborhood}
+              />
+            </div>
+
+            <div className="signup-input-div">
+              <label> Email: </label>
+              <input
+                className="signup-input email"
+                type="text"
+                placeholder="Enter Email"
+                value={this.state.cookSignUpEmail}
+                onChange={this.updateSignUpEmail}
+              />
+            </div>
+
+            <div className="signup-input-div">
+              <label> Username: </label>
+              <input
+                className="signup-input username"
+                type="text"
+                placeholder="Enter Username"
+                value={this.state.cookSignUpUsername}
+                onChange={this.updateSignUpUsername}
+              />
+            </div>
+
+            <div className="signup-input-div">
+              <label> Password: </label>
+              <input
+                className="signup-input password"
+                type="text"
+                placeholder="Enter Password"
+                value={this.state.cookSignUpPassword}
+                onChange={this.updateSignUpPassword}
+              />
+            </div>
+            <div className="button-container">
+              <button onClick={this.handleCookCreation}>Sign Up</button>
+            </div>
           </div>
-
-          <div className="signup-neighborhood-div">
-            <label> Neighborhood: </label>
-            <input
-              className="signup-input neighborhood"
-              type="text"
-              placeholder="Enter Neighborhood"
-              value={this.state.cookSignUpNeighborhood}
-              onChange={this.updateSignUpNeighborhood}
-            />
-          </div>
-
-          <div className="signup-email-div">
-            <label> Email: </label>
-            <input
-              className="signup-input email"
-              type="text"
-              placeholder="Enter Email"
-              value={this.state.cookSignUpEmail}
-              onChange={this.updateSignUpEmail}
-            />
-          </div>
-
-          <div className="signup-username-div">
-            <label> Username: </label>
-            <input
-              className="signup-input username"
-              type="text"
-              placeholder="Enter Username"
-              value={this.state.cookSignUpUsername}
-              onChange={this.updateSignUpUsername}
-            />
-          </div>
-
-          <div className="signup-password-div">
-            <label> Password: </label>
-            <input
-              className="signup-input password"
-              type="text"
-              placeholder="Enter Password"
-              value={this.state.cookSignUpPassword}
-              onChange={this.updateSignUpPassword}
-            />
-          </div>
-
-          <button onClick={this.handleCookCreation}>Sign Up</button>
-
         </div>
         </div>
 
         <div className="signup-photo-div">
+      {/*photo taken from The Body Book*/}
         </div>
 
       </container>
