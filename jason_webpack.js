8e0a6e05932e86969777368878b93dd0ebf84f75
Scott McCord <scott.sdm27@gmail.com>
2016-12-14 05:38:06 -0500

    switched out webpacks

---
+++ jason_webpack.js
@@ -0,0 +1,71 @@
+// const webpack           = require('webpack');
+// const path              = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const htmlTemplate      = require('html-webpack-template');
+
+// const BUILD_DIR         = path.resolve(__dirname, 'dist');
+// const APP_DIR           = path.resolve(__dirname, 'src');
+
+// const config = {
+//   entry:  `${APP_DIR}/main.js`,
+//   output: {
+//     path:     BUILD_DIR,
+//     filename: '/js/[name].js',
+//   },
+//   cache:   true,
+//   debug:   true,
+//   devtool: 'eval-source-map',
+//   stats:   {
+//     colors:  true,
+//     reasons: true,
+//   },
+//   resolve: {
+//     extensions: ['', '.js', '.jsx'],
+//   },
+//   plugins: [
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+//       },
+//     }),
+//     new HtmlWebpackPlugin({
+//       title:      "Home Cookin'",
+//       xhtml:      true,
+//       inject:     false,
+//       template:   htmlTemplate,
+//       appMountId: 'root-container',
+//     }),
+//     new ExtractTextPlugin('/css/[name].css', {
+//       allChunks: true,
+//     }),
+//   ],
+
+//   module: {
+//     loaders: [
+//       { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+//       { test: /\.(png|gif|jpg)$/, loader: 'file-loader?name=/images/[name].[ext]' },
+//       { test: /\.ico$/, loader: 'file-loader?name=/[name].[ext]' },
+//       { test: /\.jsx?$/, loader: 'babel' },
+//       {
+//         test:   /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+//         loader: 'url-loader?limit=100&mimetype=application/font-woff&name=/fonts/[name].[ext]',
+//       },
+//       {
+//         test:   /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+//         loader: 'url-loader?limit=100&mimetype=application/octet-stream&name=/fonts/[name].[ext]',
+//       },
+//       {
+//         test:   /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+//         loader: 'file-loader?name=/fonts/[name].[ext]',
+//       },
+//       {
+//         test:   /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+//         loader: 'url-loader?limit=100&mimetype=image/svg+xml&name=/fonts/[name].[ext]',
+//       },
+//     ],
+//   },
+// };
+
+
+// module.exports = config;
